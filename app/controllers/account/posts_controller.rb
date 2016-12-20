e1acd0a47b962cb9f7064b17fe15e8c6047a8961
zhangyan <tongkusat123@gmail.com>
2016-12-20 16:58:43 +0800

    my posts need to implement delete function

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,8 +2,23 @@ class Account::PostsController < ApplicationController
 before_action :authenticate_user!
 
 def index
-
 @posts = current_user.posts
 end
+
+
+def edit
+@post = Post.find(params[:id])
+end
+
+
+def destroy
+@post = Post.find(params[:id])
+@post.destroy
+flash[:alert] ="Post deleted!"
+redirect_to root_path
+
+end
+
+
 
 end
